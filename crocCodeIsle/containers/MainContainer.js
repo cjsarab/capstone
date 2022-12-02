2327475132750b6ec4ed519e77dedd7cb5ea0c4c
Cammy Smith <cjsarab1991@gmail.com>
2022-12-02 21:10:59 +0000

    Added a stylesheet component and separated out logo and name entry into their own components. No logic added

--- crocCodeIsle/containers/MainContainer.js
+++ crocCodeIsle/containers/MainContainer.js
@@ -1,43 +1,18 @@
 import React from 'react';
 import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import NameEntry from '../components/ui/NameEntry';
+import Logo from '../components/logo/Logo';
+import Style from '../components/styles/Style';
 
 const MainContainer = () => {
   return (
 
-    <View style={styles.view}>
-      <Text style={styles.text}>Croc Code Isle</Text>
-      <TouchableOpacity style={styles.field}>
-        <TextInput style={styles.input} placeholder="Please Enter Your Name..." />
-        <TextInput style={styles.input} placeholder="Please Enter Your Country..." />
-      </TouchableOpacity>
+    <View style={Style.mainContainerView}>
+        <Logo />
+        <NameEntry />
     </View>
 
   );
 };
-
-const styles = StyleSheet.create({
-    view: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: 'darkslateblue'
-    },
-    text: {
-      fontSize: 33,
-      color: 'lime'
-    },
-    input: {
-        height: 50,
-        padding: 8,
-        fontSize: 16,
-        color: 'red',
-        backgroundColor: 'white',
-        borderRadius: 50
-    },
-    field: {
-        padding: 12
-    }
-    
-  });
 
 export default MainContainer;
