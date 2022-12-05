5603dd58ea42dd41dc66ab0cd7b99f10bd328227
Cammy Smith <cjsarab1991@gmail.com>
2022-12-05 14:49:02 +0000

    nameentry

--- crocCodeIsle/components/ui/NameEntry.js
+++ crocCodeIsle/components/ui/NameEntry.js
@@ -3,10 +3,20 @@ import { View, TextInput } from 'react-native';
 import Style from '../styles/Style';
 
 
-const NameEntry = () => {
+const NameEntry = ({name, setName, addName}) => {
+
+  const submitName = (text) => {
+    addName(text)
+  }
+
+
+
   return (
     <View style={Style.field}>
-        <TextInput style={Style.input} placeholder="Your Name..." />
+        <TextInput 
+        style={Style.input}
+        onSubmitEditing={(event) => submitName(event.nativeEvent.text)}
+        placeholder="Your Name..." />
     </View>
   );
 };
--- crocCodeIsle/containers/MainContainer.js
+++ crocCodeIsle/containers/MainContainer.js
@@ -1,15 +1,28 @@
 import React from 'react';
-import { View } from 'react-native';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import {View, Text} from 'react-native';
 import NameEntry from '../components/ui/NameEntry';
 import Logo from '../components/logo/Logo';
 import Style from '../components/styles/Style';
 
 const MainContainer = () => {
+
+  const [name, setName] = useState ("")
+
+
+  function addName(name) {
+    console.log(name)
+    tempName = setName(name);
+  };
+
+
   return (
 
     <View style={Style.mainContainerView}>
         <Logo />
-        <NameEntry />
+        <NameEntry name={name} setName={setName} addName={addName}/>
+        <Text> State: {name} </Text>
     </View>
 
   );
