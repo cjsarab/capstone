b3cea5d678f2016ee52a5f2a83b42e6786d9699b
Cammy Smith <cjsarab1991@gmail.com>
2022-12-05 21:31:30 +0000

    edited gitignore

---
+++ crocCodeIsle/components/setup/LanguageSelector.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { View, Text, Pressable, StyleSheet } from 'react-native';
+import Style from '../styles/Style';
+
+
+const LanguageSelector = ({chooseLanguage, pressButton, depressButton}) => {
+
+  const onPress = (text) => {
+    console.log(text)
+    chooseLanguage(text)
+  }
+
+  const python = "Python";
+  const javascript = "Javascript";
+  const java = "Java";
+
+
+  return (
+    <View style={Style.field}>
+
+      <Pressable style={({ pressed }) => pressed ? [styles.languageChoiceButton, styles.pressed] : styles.languageChoiceButton } 
+          onPress={(event) => onPress(python)}>
+        <Text style={Style.text}>Python</Text>
+      </Pressable>
+
+      <Pressable style={({ pressed }) => pressed ? [styles.languageChoiceButton, styles.pressed] : styles.languageChoiceButton } 
+          onPress={(event) => onPress(javascript)}>
+        <Text style={Style.text}>JavaScript</Text>
+      </Pressable>
+
+      <Pressable style={({ pressed }) => pressed ? [styles.languageChoiceButton, styles.pressed] : styles.languageChoiceButton } 
+          onPress={(event) => onPress(java)}>
+        <Text style={Style.text}>Java</Text>
+      </Pressable>
+
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+
+  languageChoiceButton: {
+    backgroundColor: 'green',
+    width: 333,
+    padding: 20,
+    margin: 20,
+    borderRadius: 50,
+    borderWidth: 3,
+    borderColor: 'black',
+    textAlign: 'center'
+  },
+
+  pressed: {
+    backgroundColor: 'lime'
+  }
+
+})
+
+export default LanguageSelector;
---
+++ crocCodeIsle/components/setup/NameEntry.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View, TextInput } from 'react-native';
+import Style from '../styles/Style';
+
+
+const NameEntry = ({addName}) => {
+
+  const submitName = (text) => {
+    addName(text)
+  }
+
+  return (
+    <View style={Style.field}>
+        <TextInput 
+        style={Style.input}
+        onSubmitEditing={(event) => submitName(event.nativeEvent.text)}
+        placeholder="Your Name..." />
+    </View>
+  );
+};
+
+export default NameEntry;
