7e1b3c1c7e589066a235febc06697a1790277e7b
Louise McCafferty <mccafferty_@hotmail.com>
2022-12-05 11:07:17 +0000

    commit

--- crocCodeIsle/components/logo/Logo.js
+++ crocCodeIsle/components/logo/Logo.js
@@ -4,7 +4,7 @@ import Style from '../styles/Style';
 
 const Logo = () => {
   return (
-    <Text style={Style.logo}>Croc Code Island</Text>
+    <Text style={Style.logo}>Croc Code Islands</Text>
   );
 };
 
